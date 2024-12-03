5dab7f32be9123364e25e192717d6781ed4225da
MatsunagaAtsuki <matsuatk@outlook.jp>
2024-12-03 10:17:39 +0900

    firebase設定

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,10 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:inventory_manager/pages/login/splash.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(const MyApp());
 }
 
@@ -13,7 +16,6 @@ class MyApp extends StatelessWidget {
       title: "inventory",
       //home: LoginPage(),
       home: SplashPage(),
-
     );
   }
 }
